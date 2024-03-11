4846d2497a7caaec877a5e3e61896f6dbef18b9d
CodeAxle <avisheeljambhulkar@gmail.com>
2024-03-12 02:59:53 +0530

    updated

--- main.py
+++ main.py
@@ -23,55 +23,55 @@ import requests
 #         os.environ['MONGO_DB_URL']=env_config['MONGO_DB_URL']
 
 
-# app = FastAPI()
-# origins = ["*"]
+app = FastAPI()
+origins = ["*"]
 
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=origins,
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
-# @app.get("/", tags=["authentication"])
-# async def index():
-#     return RedirectResponse(url="/docs")
+@app.get("/", tags=["authentication"])
+async def index():
+    return RedirectResponse(url="/docs")
 
-# @app.get("/train")
-# async def train_route():
-#     try:
+@app.get("/train")
+async def train_route():
+    try:
 
-#         train_pipeline = TrainPipeline()
-#         if train_pipeline.is_pipeline_running:
-#             return Response("Training pipeline is already running.")
-#         train_pipeline.run_pipline()
-#         return Response("Training successful !!")
-#     except Exception as e:
-#         return Response(f"Error Occurred! {e}")
+        train_pipeline = TrainPipeline()
+        if train_pipeline.is_pipeline_running:
+            return Response("Training pipeline is already running.")
+        train_pipeline.run_pipline()
+        return Response("Training successful !!")
+    except Exception as e:
+        return Response(f"Error Occurred! {e}")
 
-# @app.get("/predict")
-# async def predict_route(request:Response,file: UploadFile =TRAIN_FILE_NAME ):
-#     try:
-#         # get data from user csv file
-#         # conver csv file to dataframe
-#         # df = pd.read_csv(file.file)
-#         df = pd.read_csv(TRAIN_FILE_NAME)
-#         model_resolver = ModelResolver(model_dir=SAVED_MODEL_DIR)
-#         if not model_resolver.is_model_exists():
-#             return Response("Model is not available")
+@app.get("/predict")
+async def predict_route(request:Response,file: UploadFile =TRAIN_FILE_NAME ):
+    try:
+        # get data from user csv file
+        # conver csv file to dataframe
+        # df = pd.read_csv(file.file)
+        df = pd.read_csv(TRAIN_FILE_NAME)
+        model_resolver = ModelResolver(model_dir=SAVED_MODEL_DIR)
+        if not model_resolver.is_model_exists():
+            return Response("Model is not available")
         
-#         best_model_path = model_resolver.get_best_model_path()
-#         model = load_object(file_path=best_model_path)
-#         y_pred = model.predict(df)
-#         df['predicted_column'] = y_pred
-#         df['predicted_column'].replace(TargetValueMapping().reverse_mapping(),inplace=True)
-#         # return df.to_html()
-#         #decide how to return file to user.
+        best_model_path = model_resolver.get_best_model_path()
+        model = load_object(file_path=best_model_path)
+        y_pred = model.predict(df)
+        df['predicted_column'] = y_pred
+        df['predicted_column'].replace(TargetValueMapping().reverse_mapping(),inplace=True)
+        # return df.to_html()
+        #decide how to return file to user.
         
-#     except Exception as e:
-#         raise Response(f"Error Occured! {e}")
+    except Exception as e:
+        raise Response(f"Error Occured! {e}")
     
 def main():
     try:
@@ -84,10 +84,4 @@ def main():
 if __name__=="__main__":
     #main()
     # set_env_variable(env_file_path)
-    #app_run(app, host=APP_HOST, port=APP_PORT)
-    try:
-        training_pipeline = TrainPipeline()
-        training_pipeline.run_pipline()
-    except Exception as e:
-        print(e)
-    
+    app_run(app, host=APP_HOST, port=APP_PORT)
